888895546bee052e1462c15adead21b32f8ede7e
swayamsidha <chandan20july1997@gmail.com>
2020-05-31 22:24:46 +0530

    react app init

---
+++ src/components/navigation.jsx
@@ -0,0 +1,23 @@
+import {Router, Link} from 'react-router-dom'
+import React from 'react'
+class Navigation extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+
+        }
+    }
+
+
+
+
+    render(){
+        return(
+
+            <div>
+                
+            </div>
+        )
+    }
+}
